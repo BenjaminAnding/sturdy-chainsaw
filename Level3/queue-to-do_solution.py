7f741b63708de5c7617e0d8954f724fddca1960a
Ben Anding <benanding@Bens-MacBook-Air.local>
2021-11-15 00:46:56 -0600

    queue-to-do

--- Level3/queue-to-do_solution.py
+++ Level3/queue-to-do_solution.py
@@ -1,12 +1,2 @@
-def solution(di, ci, tc):
+def solution():
     #your code here
-    damage = 550
-    crit = .05
-    crits = (crit+ci)*tc
-    return ((damage+di)*(tc-crits)+(((damage+di)*2)*crits))
-casts = 100
-base = solution(0,0, casts)
-critincrease = solution(0,.01, casts)
-dmgincrease = solution(5,0, casts)
-print(base, critincrease, dmgincrease)
-print(critincrease-base, dmgincrease-base)
