bf6fa35aa3ad7743be5e7b1c46b2176d45796533
Ben Anding <benanding@Bens-MacBook-Air.local>
2021-11-15 02:52:24 -0600

    queue-to-do done

--- Level3/queue-to-do_solution.py
+++ Level3/queue-to-do_solution.py
@@ -1,2 +1,18 @@
-def solution():
-    #your code here
+from functools import reduce
+from queue import Queue
+def solution(s, l):
+	q = Queue(maxsize = l)
+	workers = [x for x in range(s, s + (l ** 2))]
+	xs = []
+	i = 0
+	j = l
+	while i < len(workers):
+		while not q.full():
+			q.put(workers[i])
+			i += 1
+		for k in range(j):
+			xs.append(q.get())
+		while not q.empty():
+			q.get()
+		j -= 1
+	return reduce(lambda x, y: x ^ y, xs)
